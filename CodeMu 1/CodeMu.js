294c27e790aa4b0610b2feac2a1dfbf2121ae4a4
RasulovBoburjon <boburgithub@gmail.com>
2022-03-13 20:49:29 +0500

    proticajs

---
+++ CodeMu 1/CodeMu.js
@@ -0,0 +1,313 @@
+
+                /*  Vazifa 1 - js. 
+  O'zgaruvchi yarating num va unga qiymat bering 123.
+ Funktsiyadan foydalanib, ushbu o'zgaruvchining qiymatini ekranga chop eting alert. */
+/*
+ let num = 123;
+ alert(num);
+*/
+
+/*  Vazifa 2 js
+
+O'zgaruvchilarni e'lon qilishning barcha tavsiflangan usullarini o'zingiz sinab ko'ring.*/
+/*
+let dem = 123,
+    a = "121",
+    v = "Bobur",
+    f = 12;
+demo = dem + a + v + f;
+  alert(demo);
+
+  */
+
+  /* Vazifa 3js 
+
+O'zgaruvchi yarating a. Unga qiymatni yozing, 
+uni 10ekranda ko'rsating. Keyin unga qiymatni yozing, uni 20ekranda ko'rsating.   */
+/*
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+*/
+
+//JavaScript-da raqamlar va ular ustida amallar
+
+ /*  
+Vazifa 1 js
+O'zgaruvchi yarating a. Unga sonlar yig'indisini yozing 1va 2. 
+Ekranda o'zgaruvchining mazmunini ko'rsatish uchun 3funksiyadan foydalaning . alerta */
+
+/*
+let a = 1 + 2;
+myBb(a);
+function myBb(a){
+alert(a);
+}
+*/
+
+/*                  Vazifa 2js
+
+alert Raqamlar yig'indisini chiqarish uchun funktsiyadan 1 foydalaning 2 va 3.   
+
+function myNumber(a,b){
+    return a + b;
+}
+alert(myNumber(2,3));
+*/
+
+/* Vazifa 3-js.
+
+Qiymatli o'zgaruvchi va a qiymatli o'zgaruvchi yarating . 
+Ularning yig'indisini, ayirmasini,
+ ko'paytmasini va qismini (bo'lish natijasi) ekranda chop eting. 10 b 2  */
+/*
+ let a = 12 - 20 + 30 * 40 / 4;
+ alert(a);
+*/
+
+/*  Vazifa 4--js
+
+Qiymatli o'zgaruvchi va cqiymatli o'zgaruvchi yarating . 
+Ularni jamlang va natijani o'zgaruvchiga belgilang .
+ O'zgaruvchining qiymatini ekranda ko'rsatish . 10 d 5 result result  */
+    /*
+  let result = 10 / 2 * 100;
+  alert(result);
+*/
+
+
+ /* Vazifa 5--js
+
+a Qiymat o'zgaruvchisi, qiymat o'zgaruvchisi 1 va b qiymat
+ 2 o'zgaruvchisini yarating . Ularning yig'indisini ekranda ko'rsating. c 3  */ 
+ /*
+    let a = 1,
+        b = 2,
+        c = 3;
+        v = a + b + c;
+    alert(v);
+*/
+
+ /*  Vazifa 6=--=-js
+
+Qiymatli o'zgaruvchi va a qiymatli o'zgaruvchi yarating . 
+O'zgaruvchidan olib tashlang va natijani o'zgaruvchiga belgilang . 10 b 5 a b c
+Keyin o'zgaruvchi yarating, dunga qiymat bering 7.
+ va o'zgaruvchilarni qo'shing c va d natijani o'zgaruvchiga yozing result. 
+O'zgaruvchining qiymatini ekranda ko'rsatish result.  */
+/*
+let b = 10;
+    a = 5;
+    c = 5;
+    b = 2;
+    result = b + a + c + b;
+    alert(result);
+  */
+ 
+    /*  Vazifa 7====js
+    Kodni ishga tushirmasdan, 
+  ekranda nima ko'rinishini aniqlang:
+
+let a = 5 + 5 * 3;
+alert(a); 
+     */
+
+
+/* Vazifa 8js.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 5 + 5 * 3 + 3;
+alert(a);  */
+
+/*  9- topshiriqjs.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 8 / 2 + 2;
+alert(a);  */
+
+/*     10 -topshiriq js.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 8 + 2 / 2;
+alert(a);  */
+
+/* 13- topshiriqjs.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = (2 + 3) * (2 + 3);
+alert(a);  
+
+14 -topshiriqjs.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = (2 + 3) * 2 + 3;
+alert(a);
+
+15 -topshiriqjs.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 2 * (2 + 4 * (3 + 1));
+alert(a);
+
+
+16- topshiriqjs.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 2 * 8 / 4;
+alert(a);
+Muammo 17js.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = (2 * 8) / 4;
+alert(a);
+Muammo 18js
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 2 * (8 / 4);
+alert(a);
+*/
+
+//Kasrlar
+
+/*   Muammo 19__js
+
+Raqamni o'zgaruvchiga, araqamni esa o'zgaruvchiga yozing . 
+Ushbu o'zgaruvchilar
+ qiymatlarining yig'indisini toping va uni ekranda ko'rsating. 1.5 b 0.75  */
+
+
+// alert(1.5 + 0.75);
+
+/*                Salbiy raqamlar
+Raqamlar salbiy bo'lishi mumkin. Buning uchun raqam oldiga minus belgisini qo'ying:
+
+let a = -1;
+alert(a); // выведет -1 
+ */
+
+           /*   Muammo 20js
+
+a Qiymatli o'zgaruvchi yarating -100. Ushbu qiymatni ekranga chop eting.  
+ */ 
+
+//let a = - 100; alert(a);
+
+
+/* Muammo 21js.
+
+O'zgaruvchi yarating, unga aijobiy yoki manfiy sonlarni yozing. 
+Ushbu raqamni ekranga qarama-qarshi belgi bilan chop eting.    */
+
+//let a = 100; alert(a);  b = -100;   alert(b);
+  
+/* Muammo 22js.Pm.Bs.Nm
+
+Qiymatli o'zgaruvchi va a qiymatli 10 o'zgaruvchi berilgan .
+ ga bo'lingandan keyin qoldiqni toping . b 3 a b   */
+/*
+ let a = 10;
+     b = 3;
+    res = a % b;
+    alert(res);
+    alert(10 ** 3); // выведет 1000
+
+    */
+
+    /*  Muammo 23 js
+  2 Raqamni uchinchi darajaga ko'taring 10. Natijani ekranda ko'rsating.  
+*/
+
+//alert(2 ** 10);
+
+/*  Eksponentsiya ustuvorligi
+Ko'rsatkichni oshirish amali ko'paytirish va bo'lishdan ustun turadi. Quyidagi misol avval eksponentlanadi va keyin ko'payadi:
+
+alert(2 * 2 ** 3);
+Muammo 24js
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 3 * 2 ** 3;
+alert(a);   */
+
+
+/* Muammo 25js.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = (3 * 2) ** 3;
+alert(a);
+Muammo 26js.
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 3 * 2 ** (3 + 1);
+alert(a);
+Muammo 27js
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 2 ** 3 * 3;
+alert(a);
+Muammo 28js
+
+Kodni ishga tushirmasdan, ekranda nima ko'rinishini aniqlang:
+
+let a = 3 * 2 ** 3 * 3;
+alert(a);   */
+
+/*   null qiymat
+Qiymat null"hech narsa" degan ma'noni anglatadi. Misol uchun, biz o'zgaruvchiga u erda hech narsa yo'qligi belgisi sifatida quyidagi qiymatni belgilashimiz mumkin:
+
+let a = null;
+alert(a); // выведет null 
+
+let b =;
+alert(b); // выведет null
+*/
+
+/* haqiqiy va noto'g'ri qiymatlar
+Qiymatlar mos ravishda rost trueva falseyolg'onni bildiradi. Ular ikkita javobni talab qiladigan narsalar uchun ishlatiladi - ha yoki yo'q.
+
+Vazifa 3js.
+
+O'zgaruvchiga qiymatni tayinlang true. Ushbu o'zgaruvchini ekranda ko'rsating.
+
+Vazifa 4js.
+
+O'zgaruvchiga qiymatni tayinlang false. Ushbu o'zgaruvchini ekranda ko'rsating.
+
+NaN qiymati
+Qiymat NaN( No-A-Number ) raqamni bildirmaydi . Bu qiymat matematik amallarni raqamlar bilan emas, balki boshqa narsa bilan bajarishga harakat qilganda olinadi. Masalan, satrni raqamga ko'paytirishga urinish bunday qiymatni beradi:
+
+alert('abc' * 3); // выведет NaN
+Har qanday operatsiyalar NaNnatija hali ham bo'lishiga olib keladi NaN:
+
+alert(NaN + 1); // выведет NaN  
+  */
+
+
+/* Vazifa 6 js
+
+ Raqamni ga 10bo'ling 0. Natijani ekranda ko'rsating.
+
+Vazifa 7 js.
+
+ Raqamni ga -10bo'ling 0. Natijani ekranda ko'rsating.
+    */
+
+// alert(10 / 0);
+ //alert(-10 / 0);
+
+
+ 
